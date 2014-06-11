44eef65731490afb3fd1b94b2e9e03e9d68e96ee
Ramy Khater <eng.ramymohie@gmail.com>
2014-06-11 16:39:50 +0300

    adding building script

---
+++ build_git_with_openssl.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# gnutls package. It's working weird behind a proxy. But openssl is working fine even in weak network. So workaround is that we should compile git with# openssl.
+# -------------------------------------------------------------------------
+# Version 1.0 (June 11th 2014)
+# -------------------------------------------------------------------------
+# Copyright (c) 2012 Ramy Khater <eng.ramymohie@gmail.com>
+# This script is licensed under GNU GPL version 2.0 or above
+# -------------------------------------------------------------------------
+
+sudo apt-get install build-essential fakeroot dpkg-dev
+sudo rm -fr ~/.git-openssl
+mkdir ~/.git-openssl
+cd ~/.git-openssl
+sudo apt-get source git
+sudo apt-get build-dep git
+sudo apt-get install libcurl4-openssl-dev
+sudo dpkg-source -x git_*.dsc
+cd git-*
+
+sleep 5
+echo "--> replace all instances of libcurl4-gnutls-dev with libcurl4-openssl-dev in debian/control."
+sudo sed -i 's/libcurl4-gnutls-dev/libcurl4-openssl-dev/g' debian/control
+sudo dpkg-buildpackage -rfakeroot -b
+
+sudo dpkg -i git_*amd64.deb
